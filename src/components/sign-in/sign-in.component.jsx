5d8624b0ae836463ce4c4f1ba4ab9c4369f5084f
Atul.Arora <atul.arora@in.imshealth.com>
2020-05-21 16:08:10 +0530

    integrated firebase google sign in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
+import { signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component{
 
@@ -52,9 +53,15 @@ class SignIn extends React.Component{
                         required 
                         label="Password"
                     />
-                    <CustomButton type="submit">
-                    Sign In
-                    </CustomButton>
+                    <div className='buttons'>
+                        <CustomButton type="submit">
+                            Sign In
+                        </CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                            Sign In With Google
+                        </CustomButton>
+                    </div>
+                   
                 </form>
             </div>
         );
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,30 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const config = {
+    apiKey: "AIzaSyBj3Pum1ErQ2yKKqrTerHmBHOEP3WqSSQM",
+    authDomain: "crwn-db-942a6.firebaseapp.com",
+    databaseURL: "https://crwn-db-942a6.firebaseio.com",
+    projectId: "crwn-db-942a6",
+    storageBucket: "crwn-db-942a6.appspot.com",
+    messagingSenderId: "617852298938",
+    appId: "1:617852298938:web:83c7f7df0ecb741ce60483",
+    measurementId: "G-L8E1MXP1C0"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+provider.setCustomParameters({prompt: 'select_account'});
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default provider;
+
+
+
