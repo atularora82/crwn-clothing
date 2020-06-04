92fe357c6107ca5b0d6e2fb014f713ed04c89456
Atul.Arora <atul.arora@in.imshealth.com>
2020-06-04 17:50:01 +0530

    added redux layer

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -24,7 +24,7 @@ class SignUp extends React.Component{
         event.preventDefault();
         const {displayName, email, password, confirmPassword} = this.state;
         if(password !== confirmPassword){
-            alert("Password dont match");
+            alert("Password don't match");
             return;
         }
 
